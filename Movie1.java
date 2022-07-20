97de0566a14d626cc8b21f8f670faaa39dcef31d
Sabaritesting <108785595+Sabaritesting@users.noreply.github.com>
2022-07-20 18:12:12 +0530

    Add files via upload

---
+++ Movie1.java
@@ -0,0 +1,17 @@
+package abstraction;
+
+public class Movie1 extends Movie{
+	public void title() {
+		System.out.println("Bombay");
+	}
+	public void director() {
+		System.out.println("Mani Rathnam");
+	}
+	public void cast() {
+		System.out.println("AravindSwamy,Manisha ,Prakash Raj,Naasar");
+	}
+	public void music() {
+		System.out.println("AR Rahmaan");
+	}
+
+}
