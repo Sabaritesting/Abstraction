3ff3837a8236be4d43cde9d39e25316bb560c876
Sabaritesting <108785595+Sabaritesting@users.noreply.github.com>
2022-07-20 18:12:32 +0530

    Add files via upload

---
+++ Testmovie.java
@@ -0,0 +1,14 @@
+package abstraction;
+
+public class Testmovie {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Movie1 a=new Movie1();
+a.title();
+a.director();
+a.cast();
+a.music();
+	}
+
+}
