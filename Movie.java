5fb44d7fb76a199e0d15a9f29dc2beae3dab527a
Sabaritesting <108785595+Sabaritesting@users.noreply.github.com>
2022-07-20 18:11:49 +0530

    Add files via upload

---
+++ Movie.java
@@ -0,0 +1,11 @@
+package abstraction;
+
+public abstract class Movie {
+public abstract void title();
+public abstract void director();
+  
+
+public void cast() {
+	System.out.println("Hero, Heroine, Side Artist. Villan");
+}
+}
